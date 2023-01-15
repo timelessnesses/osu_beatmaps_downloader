05496d47c27ecb09041908283c3f767ac41cc5c5
timelessnesses <mooping3roblox@gmail.com>
2023-01-15 14:30:19 +0700

    har

---
+++ Form1.cs
@@ -0,0 +1,9 @@
+namespace osu_downloader;
+
+public partial class Form1 : Form
+{
+    public Form1()
+    {
+        InitializeComponent();
+    }
+}
--- Makefile
+++
@@ -1,6 +0,0 @@
-.PHONY: install_deps
-
-install_deps:
-	$(RUN)
-build:
-	$(BUILD) .
